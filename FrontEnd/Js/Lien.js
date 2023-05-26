3bc1497b2fbdd186d80e73e09b846861899c79f7
SofianAbd <sofian.move@hotmail.com>
2023-05-26 14:01:48 +0200

    fin de la galerie début des filtres

--- FrontEnd/Js/Lien.js
+++ FrontEnd/Js/Lien.js
@@ -4,26 +4,29 @@ fetch('http://localhost:5678/api/categories')
         const categories = data.map(id => id.name);
         console.log(categories);});
 
-const projetsPromise = fetch('http://localhost:5678/api/works')
-.then(response => response.json());
-      
-projetsPromise.then(projetsPromise => projets.json);
+let projets
+async function fetchProjets() {
+    return await fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => { projets = data});
+}
 
-function genererProjets(projets) {
+async function genererProjets() {
+    await fetchProjets();
+    console.log(fetchProjets());
     for (let i = 0; i < projets.length; i++) {
         console.log(i);
         const projetElement = document.createElement("figure");
         const titleElement = document.createElement('figcaption');
         const imageElement = document.createElement('img');
         const galleryElement = document.querySelector('.gallery');
-        imageElement.src = projets[i].imageurl;
+        imageElement.src = projets[i].imageUrl;
         titleElement.textContent = projets[i].title;
         projetElement.appendChild(imageElement);
         projetElement.appendChild(titleElement);
         galleryElement.appendChild(projetElement);
-        console.log(titleElement.textContent);
         document.querySelector('.gallery').appendChild(projetElement);
     }
 }
-
-genererProjets(projets);
+fetchProjets();
+genererProjets();
