1e2d4156fe9eeb7a2757aa8fc125bf7705fb3e98
SofianAbd <sofian.move@hotmail.com>
2023-06-08 17:45:28 +0200

    Update Lien.js

--- FrontEnd/Js/Lien.js
+++ FrontEnd/Js/Lien.js
@@ -86,4 +86,35 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchCategories();
 
     genererBoutons();
+
+
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Empêche la soumission du formulaire par défaut
+
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+
+    const logs = {
+        email: emailValue,
+        password: passwordValue
+    };
+
+    console.log(logs);
+
+    let response = await fetch('http://localhost:5678/api/users/login', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(logs)
+                    })
+        console.log(response)
+        let result = await response.json();
+        console.log(result.token);
+
+    // Reste du code pour traiter les valeurs du formulaire, comme l'envoi des données au serveur
+});
 });
